0b37b2366e79b59e968f860f8a2c4286079cd651
ryleu <riley.j.mclain@gmail.com>
2022-05-09 20:52:54 -0400

    add version checks and powercord stuff

--- other/discord/install.sh
+++ other/discord/install.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-for v in discord discordcanary
-do
-	echo "Applying config for $v. The process will be killed if it is running"
+if [[ ! -e '/usr/share/discord-canary/resources/build_info.json' ]]; then
+	echo 'Discord Canary is not installed.'
+	exit 0
+fi
 
-	killall -I $v
+installed_canary_version=$(jq .version '/usr/share/discord-canary/resources/build_info.json' | grep -oE '[^"]+')
+latest_canary_version=$(curl -sI 'https://discord.com/api/download/canary?platform=linux&format=tar.gz' | sed -E -n '/location:/{s/^.+linux\/// ; s/\/.+$//p}')
 
-	mkdir -p ~/.config/$v
+if [[ ! $installed_canary_version = $latest_canary_version ]]; then
+	echo "Discord Canary is not up-to-date"
+else
+	echo "Discord Canary is up-to-date"
+fi
 
-	if [[ -e ~/.config/$v/settings.json ]]; then
-		python3 setmin.py ~/.config/$v/settings.json
-	else
-		echo '{"MIN_WIDTH":0,"MIN_HEIGHT":0}' > ~/.config/$v/settings.json
-	fi
-done
+echo "Applying config for Discord Canary. The process will be killed if it is running"
+killall DiscordCanary
+
+mkdir -p ~/.config/discordcanary
+
+if [[ -e ~/.config/discordcanary/settings.json ]]; then
+	python3 setmin.py ~/.config/discordcanary/settings.json
+else
+	echo '{"MIN_WIDTH":0,"MIN_HEIGHT":0}' > ~/.config/discordcanary/settings.json
+fi
--- other/discord/setmin.py
+++ other/discord/setmin.py
@@ -9,9 +9,8 @@ with open(sys.argv[1], "r") as file:
         print("Generating new file")
         c = {}
 
-with open(sys.argv[1], "w") as file:
-    if c.get("MIN_WIDTH", 1) != 0 or c.get("MIN_HEIGHT", 1) != 0:
+if c.get("MIN_WIDTH", 1) != 0 or c.get("MIN_HEIGHT", 1) != 0:
+    with open(sys.argv[1], "w") as file:
         c["MIN_WIDTH"] = 0
         c["MIN_HEIGHT"] = 0
         file.write(json.dumps(c, indent = 4, sort_keys = True) + "\n")
-    
