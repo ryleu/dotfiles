bd1a8768c4fb833ac03bb930770aef9521d276d5
ryleu <riley.j.mclain@gmail.com>
2022-07-10 14:33:05 -0400

    fixed starship config

---
+++ dirs/config/starship/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# figure out where we are
+dotfiles_dir=`dirname $(realpath $0)`
+
+echo -e "Setting up backup link"
+
+if [[ -L "$HOME/.config/starship.toml" ]]; then
+	echo "Backup link already exists"
+else
+	if [[ -e "$HOME/.config/starship.toml" ]]; then
+		echo "Backing up current starship.toml"
+		mv -f "$HOME/.config/starship.toml" "$HOME/.config/starship.toml.bak"
+	fi
+	echo "Creating backup link"
+	ln -s "$dotfiles_dir/config.toml" "$HOME/.config/starship.toml"
+fi
+
+echo "Starship backup link setup complete!"
