3ccd420a5feb755c0af872212956cf073aab4241
ryleu <riley.j.mclain@gmail.com>
2022-07-10 14:33:44 -0400

    fixed starship config part II

---
+++ dirs/config/pre-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# figure out where we are
+dotfiles_dir=`dirname $(realpath $0)`
+
+# get the dirs with install files to run
+dotfiles=`ls -d */ | grep -oE "[^/]+/\$" | tr -d "/"`
+
+for dotfile in $dotfiles
+do
+	if [[ -e "$dotfiles_dir/$dotfile/install.sh" ]]; then
+		echo "Running setup script"
+		echo
+		cd "$dotfile"
+		bash "install.sh"
+		cd ".."
+		echo
+	fi
+done
